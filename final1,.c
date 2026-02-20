ea623e7e9d3dde9ceea89daf111bdb2d8777ac7f
bttlmrym123-gif <bttlmrym123@gmail.com>
2026-02-20 20:50:39 +0300

    Update final1,.c

--- final1,.c
+++ final1,.c
@@ -2,65 +2,91 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// 1. Veri Tipleri
-typedef enum { TYPE_F32, TYPE_F16, TYPE_I8 } TensorType;
+// Desteklenen veri tipleri
+typedef enum {
+    TYPE_FLOAT32,
+    TYPE_INT8
+} TensorType;
 
-// 2. Esnek Tensör Yapýsý
+// Primitive TinyTensor yapÄ±sÄ±
 typedef struct {
     TensorType type;
-    int length;        // size_t yerine daha garanti olan int kullandým
-    float scale;
-    int8_t zero_point;
-    
+    int size;
     union {
-        float* f32_ptr;
-        uint16_t* f16_ptr;
-        int8_t* i8_ptr;
+        float* f_data;
+        int8_t* i8_data;
     } data;
 } TinyTensor;
 
-// 3. Quantization Fonksiyonu
-void quantize(TinyTensor* t, float* raw_data) {
-    int i; // Eski derleyiciler için deðiþkeni dýþarýda tanýmladým
-    for (i = 0; i < t->length; i++) {
-        // Formül: Q = (R / S) + Z
-        float quantized_val = (raw_data[i] / t->scale) + (float)t->zero_point;
-        
-        // Sýnýrlandýrma
-        if (quantized_val > 127.0f) quantized_val = 127.0f;
-        if (quantized_val < -128.0f) quantized_val = -128.0f;
-        
-        t->data.i8_ptr[i] = (int8_t)quantized_val;
+// Float -> int8 Quantization fonksiyonu
+int8_t quantize(float value, float scale, int zero_point) {
+    int32_t q = (int32_t)((value / scale) + zero_point);
+
+    if (q > 127) q = 127;
+    if (q < -128) q = -128;
+
+    return (int8_t)q;
+}
+
+// TensÃ¶r ortalama hesaplama
+float calculate_mean(TinyTensor* t) {
+    float sum = 0.0f;
+
+    for (int i = 0; i < t->size; i++) {
+        sum += t->data.f_data[i];
     }
+
+    return sum / t->size;
 }
 
 int main() {
-    float sensor_verileri[] = {0.85f, -0.42f, 0.12f, 0.99f, -0.95f};
-    int n = 5;
-    int i;
 
-    // Tensör belleði ayýrma
-    TinyTensor my_tensor;
-    my_tensor.type = TYPE_I8;
-    my_tensor.length = n;
-    my_tensor.scale = 0.01f;
-    my_tensor.zero_point = 0;
-    
-    // Bellek ayýrma ve tip dönüþümü (C++ uyumu için cast eklendi)
-    my_tensor.data.i8_ptr = (int8_t*)malloc(n * sizeof(int8_t));
+    printf("=== IoT Sicaklik Sensoru Veri Simulasyonu ===\n\n");
 
-    if (my_tensor.data.i8_ptr == NULL) {
-        printf("Bellek ayirma hatasi!\n");
-        return 1;
+    int size = 5;
+
+    // Float tensÃ¶r oluÅŸtur
+    TinyTensor sensorTensor;
+    sensorTensor.type = TYPE_FLOAT32;
+    sensorTensor.size = size;
+    sensorTensor.data.f_data = (float*)malloc(size * sizeof(float));
+
+    // SensÃ¶r verileri (Ã¶rnek)
+    float sensorValues[5] = {23.5, 24.1, 22.8, 25.0, 23.9};
+
+    for (int i = 0; i < size; i++) {
+        sensorTensor.data.f_data[i] = sensorValues[i];
     }
 
-    quantize(&my_tensor, sensor_verileri);
-
-    printf("--- TinyML Tensor Demosu ---\n");
-    for(i = 0; i < n; i++) {
-        printf("Float: %.2f -> Int8: %d\n", sensor_verileri[i], my_tensor.data.i8_ptr[i]);
+    printf("Float Sensor Verileri:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%.2f ", sensorTensor.data.f_data[i]);
     }
 
-    free(my_tensor.data.i8_ptr);
+    float mean = calculate_mean(&sensorTensor);
+    printf("\n\nOrtalama Sicaklik: %.2f\n\n", mean);
+
+    // Quantization parametreleri
+    float scale = 0.1f;
+    int zero_point = 0;
+
+    // Int8 tensÃ¶r oluÅŸtur
+    TinyTensor quantTensor;
+    quantTensor.type = TYPE_INT8;
+    quantTensor.size = size;
+    quantTensor.data.i8_data = (int8_t*)malloc(size * sizeof(int8_t));
+
+    for (int i = 0; i < size; i++) {
+        quantTensor.data.i8_data[i] = quantize(sensorTensor.data.f_data[i], scale, zero_point);
+    }
+
+    printf("Quantized Int8 Verileri:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", quantTensor.data.i8_data[i]);
+    }
+
+    free(sensorTensor.data.f_data);
+    free(quantTensor.data.i8_data);
+
     return 0;
 }
